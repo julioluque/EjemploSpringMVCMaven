38feeeaef6f7db8adc730020cec75386ce283abb
julio luque <jluque@tecnosoftware.com>
2019-10-30 11:05:17 -0300

    productos y personas

--- src/edu/curso/java/spring/bo/Producto.java
+++ src/edu/curso/java/spring/bo/Producto.java
@@ -1,5 +1,38 @@
 package edu.curso.java.spring.bo;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Producto {
+
+	private Long id;
+	private Double precio;
+	private String nombre;
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
 }
--- src/edu/curso/java/spring/service/PersonaService.java
+++ src/edu/curso/java/spring/service/PersonaService.java
@@ -1,4 +1,4 @@
-package edu.curso.java.spring.services;
+package edu.curso.java.spring.service;
 
 import java.util.List;
 
